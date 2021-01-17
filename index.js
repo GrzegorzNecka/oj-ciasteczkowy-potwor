4a0a51c99a1083cc45cf12f1f44351c5245bed4d
GrzegorzNecka <strzypior@gmail.com>
2021-01-17 16:48:40 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+
+const cookieInfo = document.querySelector(".cookie-info");
+const getCookie = () => window.sessionStorage.getItem("cookie");
+const setCookie = () => window.sessionStorage.setItem("cookie", "1");
+const checkCookieState = cookie => (cookie === "1") && (cookieInfo.style.display = "none");
+const delCookieAlert = () => checkCookieState(getCookie());
+
+
+
+const setCookieState = cookieInfo.addEventListener("click", e => {
+  if (e.target.tagName != "BUTTON") {
+    return;
+  }
+
+  setCookie();
+  delCookieAlert();
+});
+
+delCookieAlert();
