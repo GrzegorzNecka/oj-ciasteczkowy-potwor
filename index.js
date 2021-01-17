8932886d1e100c0bd0d8a32fc73f0eabb36aa3e3
GrzegorzNecka <strzypior@gmail.com>
2021-01-17 17:01:00 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 import "./style.css";
 
 const cookieInfo = document.querySelector(".cookie-info");
-const getCookie = () => window.sessionStorage.getItem("cookie");
-const setCookie = () => window.sessionStorage.setItem("cookie", "1");
-const checkCookieState = cookie => (cookie === "1") && (cookieInfo.style.display = "none");
-const delCookieAlert = () => checkCookieState(getCookie());
-
-delCookieAlert();
+const getCookie = () => window.localStorage.getItem("cookie");
+const setCookie = () => window.localStorage.setItem("cookie", "1");
+const delCookieAlert = cookie => {
+  if (cookie === "1") {
+    cookieInfo.style.display = "none";
+  }
+};
+const checkCookieAlert = () => delCookieAlert(getCookie());
 
 const setCookieState = cookieInfo.addEventListener("click", e => {
-
   if (e.target.tagName != "BUTTON") {
     return;
   }
 
   setCookie();
-  delCookieAlert();
-
+  checkCookieAlert();
 });
 
-
+checkCookieAlert();
